0d860daa3e7590e2984e80b97ede3ecfd383396f
Ognjen Belic <obelic2@gmail.com>
2018-10-18 07:52:17 +0200

    Test files conform to linter

--- test/escapeBackslash.spec.js
+++ test/escapeBackslash.spec.js
@@ -2,57 +2,57 @@ var assert = require('chai').assert;
 var stringConverter = require('../src/converter');
 
 describe('escapeBackslash', () => {
-    describe('Single Quote strings', () => {
-        it('should return handle empty string', () => {
-            assert.equal(stringConverter.escapeBackslash(''), '');
-        });
-        it('should return two backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash('\\'), '\\\\');
-        });
-        it('should return four backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash('\\\\'), '\\\\\\\\');
-        });
-        it('should handle unrelated characters', () => {
-            assert.equal(stringConverter.escapeBackslash('hello'), 'hello');
-        });
-        it('should handle unrelated characters intertwined', () => {
-            assert.equal(stringConverter.escapeBackslash('hello\\something\\'), 'hello\\\\something\\\\');
-        });
+  describe('Single Quote strings', () => {
+    it('should return handle empty string', () => {
+      assert.equal(stringConverter.escapeBackslash(''), '');
     });
+    it('should return two backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash('\\'), '\\\\');
+    });
+    it('should return four backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash('\\\\'), '\\\\\\\\');
+    });
+    it('should handle unrelated characters', () => {
+      assert.equal(stringConverter.escapeBackslash('hello'), 'hello');
+    });
+    it('should handle unrelated characters intertwined', () => {
+      assert.equal(stringConverter.escapeBackslash('hello\\something\\'), 'hello\\\\something\\\\');
+    });
+  });
 
-    describe('Literal strings', () => {
-        it('should return handle empty string', () => {
-            assert.equal(stringConverter.escapeBackslash(""), "");
-        });
-        it('should return two backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash("\\"), "\\\\");
-        });
-        it('should return four backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash("\\\\"), "\\\\\\\\");
-        });
-        it('should handle unrelated characters', () => {
-            assert.equal(stringConverter.escapeBackslash("hello"), "hello");
-        });
-        it('should handle unrelated characters intertwined', () => {
-            assert.equal(stringConverter.escapeBackslash("hello\\something\\"), "hello\\\\something\\\\");
-        });
+  describe('Literal strings', () => {
+    it('should return handle empty string', () => {
+      assert.equal(stringConverter.escapeBackslash(''), '');
     });
+    it('should return two backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash('\\'), '\\\\');
+    });
+    it('should return four backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash('\\\\'), '\\\\\\\\');
+    });
+    it('should handle unrelated characters', () => {
+      assert.equal(stringConverter.escapeBackslash('hello'), 'hello');
+    });
+    it('should handle unrelated characters intertwined', () => {
+      assert.equal(stringConverter.escapeBackslash('hello\\something\\'), 'hello\\\\something\\\\');
+    });
+  });
 
-    describe('Literal strings', () => {
-        it('should return handle empty string', () => {
-            assert.equal(stringConverter.escapeBackslash(``), ``);
-        });
-        it('should return two backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash(`\\`), `\\\\`);
-        });
-        it('should return four backslashes', () => {
-            assert.equal(stringConverter.escapeBackslash(`\\\\`), `\\\\\\\\`);
-        });
-        it('should handle unrelated characters', () => {
-            assert.equal(stringConverter.escapeBackslash(`hello`), `hello`);
-        });
-        it('should handle unrelated characters intertwined', () => {
-            assert.equal(stringConverter.escapeBackslash(`hello\\something\\`), `hello\\\\something\\\\`);
-        });
+  describe('Literal strings', () => {
+    it('should return handle empty string', () => {
+      assert.equal(stringConverter.escapeBackslash(``), ``);
     });
+    it('should return two backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash(`\\`), `\\\\`);
+    });
+    it('should return four backslashes', () => {
+      assert.equal(stringConverter.escapeBackslash(`\\\\`), `\\\\\\\\`);
+    });
+    it('should handle unrelated characters', () => {
+      assert.equal(stringConverter.escapeBackslash(`hello`), `hello`);
+    });
+    it('should handle unrelated characters intertwined', () => {
+      assert.equal(stringConverter.escapeBackslash(`hello\\something\\`), `hello\\\\something\\\\`);
+    });
+  });
 });
--- test/quote.spec.js
+++ test/quote.spec.js
@@ -2,27 +2,27 @@ let assert = require('chai').assert;
 let stringConverter = require('../src/converter');
 
 describe('quote', () => {
-    const ECMA6 = "ecma6";
-    const ECMA5_SINGLE = "ecma5single";
-    const ECMA5_DOUBLE = "ecma5double";
+  const ECMA6 = 'ecma6';
+  const ECMA5_SINGLE = 'ecma5single';
+  const ECMA5_DOUBLE = 'ecma5double';
 
-	it('should return single quote', () => {
-        assert.equal(stringConverter.quote(ECMA5_SINGLE), '\'');
-    });
+  it('should return single quote', () => {
+    assert.equal(stringConverter.quote(ECMA5_SINGLE), '\'');
+  });
 
-    it('should return double quote', () => {
-        assert.equal(stringConverter.quote(ECMA5_DOUBLE), '"');
-    });
+  it('should return double quote', () => {
+    assert.equal(stringConverter.quote(ECMA5_DOUBLE), '"');
+  });
 
-    it('should not return literal quote if not a wrapping element', () => {
-        assert.equal(stringConverter.quote(ECMA6), '');
-    });
+  it('should not return literal quote if not a wrapping element', () => {
+    assert.equal(stringConverter.quote(ECMA6), '');
+  });
 
-    it('should not return literal quote if not a wrapping element is false', () => {
-        assert.equal(stringConverter.quote(ECMA6, false), '');
-    });
+  it('should not return literal quote if not a wrapping element is false', () => {
+    assert.equal(stringConverter.quote(ECMA6, false), '');
+  });
 
-    it('should return literal quote if is a wrapping element', () => {
-        assert.equal(stringConverter.quote(ECMA6, true), '`');
-    });
+  it('should return literal quote if is a wrapping element', () => {
+    assert.equal(stringConverter.quote(ECMA6, true), '`');
+  });
 });
