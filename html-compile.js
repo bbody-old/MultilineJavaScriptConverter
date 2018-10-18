8d1885249791814f5487b2843adae0056cc79a3e
Ognjen Belic <obelic2@gmail.com>
2018-10-18 07:54:11 +0200

    Compiler conforms to linter

--- html-compile.js
+++ html-compile.js
@@ -1,20 +1,20 @@
 const pug = require('pug');
 const fs = require('fs');
 const variables = require('./index.json');
-const package = require('./package.json');
+const Package = require('./package.json');
 
-variables.package = package;
+variables.package = Package;
 
-const compiledFunction = pug.compileFile('index.pug');
+// const compiledFunction = pug.compileFile('index.pug');
 
 // console.log(compiledFunction(variables));
 
 var html = pug.renderFile('index.pug', variables);
 
-fs.writeFile("dist/index.html", html, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+fs.writeFile('dist/index.html', html, function (err) {
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log("The file was saved!");
+  console.log('The file was saved!');
 });
